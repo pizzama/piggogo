ae297aa2dfe5dc60869f278db883022767f36ffc
bin.sun <bin.sun@fh.com>
2025-03-22 17:10:37 +0800

    【功能】增加首页资源

--- Assets/Script/App/Home/HomeMenuModel.cs
+++ Assets/Script/App/Home/HomeMenuModel.cs
@@ -1,5 +1,7 @@
 using SFramework.Game;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
+using System;
 
 namespace App.Home
 {
@@ -7,7 +9,31 @@ namespace App.Home
 	{
 		protected override void opening()
 		{
-			 GetData().Forget();
+			requestBaidu().Forget();
+		}
+
+		private async UniTask<bool> requestBaidu()
+		{
+			// 创建进度报告器，添加回调函数打印进度值
+			IProgress<float> progress = new Progress<float>(progressValue => {
+				// 将进度值转换为百分比并打印
+				Debug.Log($"下载进度: {progressValue * 100:F2}%");
+			});
+			
+			// 调用带有进度参数的GetData方法
+			string url = "https://www.baidu.com";
+			byte[] data = await GetData(url, progress);
+			
+			if (data != null)
+			{
+				Debug.Log($"下载完成，数据大小: {data.Length} 字节");
+			}
+			else
+			{
+				Debug.Log("下载失败");
+			}
+			await GetData();
+			return true;
 		}
 		protected override void closing()
 		{
